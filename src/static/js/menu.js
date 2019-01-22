c465869d0f2532df4041ab5ec42efbd79479c9ea
Andreas Hansson <andreas.hansson@icomera.com>
2019-01-22 17:22:40 +0100

    Changed to AmaticSC font on links
    * Added Transition transform on close menu button
    * Added .gitignore
    * etc

--- src/static/js/menu.js
+++ src/static/js/menu.js
@@ -4,11 +4,19 @@ $(document).ready(function() {
         //$(".main-container").attr("style", null);
         //$(".modal").attr("style", "display:none;");
         $(".modal").removeClass("show-menu");
+        $(".main-container").removeClass("hide-main");
     });
     $(".circle").click(function () {
         console.log("CIRCLE CLICK");
         //$(".modal").attr("style", null);
         //$(".main-container").attr("style", "display:none;");
         $(".modal").addClass("show-menu");
+        $(".main-container").addClass("hide-main");
+    });
+
+    $(".modal a").click(function() {
+        console.log("LINK CLICKED");
+        $(".modal").removeClass("show-menu");
+        $(".main-container").removeClass("hide-main");
     });
 });
